5990049e564825936175d7da20c47bd10482a84d
pakhomovi <ipakhomov.ip@gmail.com>
2017-02-13 21:02:48 +0300

    Фикстуры: инициализация и зачистка Гиперссылка
    Перенос инициализации в @BeforeMethod
    Исправил ошибку запуска GIT их директории
    "C:\Users\igor\AppData\Local\GitHub\PortableGit_f02737a78695063deace08e96d5042710d3e32db\cmd"

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/GroupCreationgTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/GroupCreationgTests.java
@@ -21,10 +21,6 @@ public class GroupCreationgTests {
     public void setUp() throws Exception {
         wd = new FirefoxDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationgTests() {
         wd.get("http://localhost/addressbook/group.php");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -33,17 +29,16 @@ public class GroupCreationgTests {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    
+    @Test
+    public void testGroupCreationg() {
+
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
         wd.findElement(By.name("group_name")).sendKeys("test1");
-      /*  if (!wd.findElement(By.xpath("//div[@id='content']//select[normalize-space(.)='[none]']//option[1]")).isSelected()) {
-            wd.findElement(By.xpath("//div[@id='content']//select[normalize-space(.)='[none]']//option[1]")).click();
-        }
-        if (!wd.findElement(By.xpath("//div[@id='content']//select[normalize-space(.)='[none]']//option[1]")).isSelected()) {
-            wd.findElement(By.xpath("//div[@id='content']//select[normalize-space(.)='[none]']//option[1]")).click();
-        }*/
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
         wd.findElement(By.name("group_header")).sendKeys("test2");
